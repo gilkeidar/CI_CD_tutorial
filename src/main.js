9c0141271451de8eeef8e02191a50a7a472eb8c1
Gil Keidar <keidar.gil@gmail.com>
2023-05-22 14:54:04 -0700

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 /**
  * Add two numbers and return their sum
- * @param {number} n 
- * @param {number} m 
+ * @param {number} n
+ * @param {number} m
  * @returns n + m
  */
-function addTwoNumbers(n, m)
-{
-    return Number(n) + Number(m);
+function addTwoNumbers (n, m) {
+  return Number(n) + Number(m)
 }
+
+const banana = 1
+const banaa2 = 2
+
+console.log(addTwoNumbers(banana, banaa2))
