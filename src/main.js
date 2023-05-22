646814f005bf454f9650945ab49d6690ad828cb5
Gil Keidar <keidar.gil@gmail.com>
2023-05-22 11:38:09 -0700

    Sample code

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Add two numbers and return their sum
+ * @param {number} n 
+ * @param {number} m 
+ * @returns n + m
+ */
+function addTwoNumbers(n, m)
+{
+    return Number(n) + Number(m);
+}
